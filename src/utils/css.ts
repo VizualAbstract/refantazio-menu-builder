6caa72e73119e162d9340c5c4f8d97679ae2411f
Corey M. Capetillo <corey.capetillo@gmail.com>
2025-04-10 07:42:42 -0700

    [Bug] Implement layer span flipping

--- src/utils/css.ts
+++ src/utils/css.ts
@@ -15,5 +15,10 @@ export const buildLayerStyles = (layer: LayerConfig, index: number) => {
   transform-style: preserve-3d;
   transform: translate(${layer.layerSettings.translateX}px, ${layer.layerSettings.translateY}px, ${layer.layerSettings.translateZ}px) rotateX(${layer.layerSettings.rotateX}deg) rotateY(${layer.layerSettings.rotateY}deg) rotateZ(${layer.layerSettings.rotateZ}deg);
   transform-origin: ${layer.layerSettings.transformOriginX}% ${layer.layerSettings.transformOriginY}% ${layer.layerSettings.transformOriginZ}%;
-}`;
+}
+  
+.layer-title-${index} span {
+  ${layer.isTextFlipped ? 'transform: rotateY(180deg);' : ''}
+}
+`;
 };
