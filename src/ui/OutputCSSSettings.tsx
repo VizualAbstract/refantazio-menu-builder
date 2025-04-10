13cc94e18a155a1fd0e0ebb6be03f4c74dddb013
Corey M. Capetillo <corey.capetillo@gmail.com>
2025-04-10 07:43:09 -0700

    [Improvement] Performance improvements

--- src/ui/OutputCSSSettings.tsx
+++ src/ui/OutputCSSSettings.tsx
@@ -1,31 +1,52 @@
-import { FC, memo, useCallback, useMemo } from 'react';
+import { FC, memo, useCallback, useEffect, useMemo, useState } from 'react';
 import { Button, Textarea, VStack } from '@chakra-ui/react';
 
 import { LayerConfig } from '../types';
 import { buildLayerStyles } from '../utils/css';
 import { copyToClipboard } from '../utils/navigator';
+import { debounce } from 'lodash';
 
 type Props = {
   config: LayerConfig[];
 };
 
 const OutputCSSSettings: FC<Props> = ({ config }) => {
+  const [debouncedConfig, setDebouncedConfig] = useState(() =>
+    config.map((layer, index) => buildLayerStyles(layer, index)).join('\n\n'),
+  );
+
   const CSSConfig = useMemo(
     () =>
       config.map((layer, index) => buildLayerStyles(layer, index)).join('\n\n'),
     [config],
   );
 
+  useEffect(() => {
+    const updateConfig = debounce(() => {
+      setDebouncedConfig(CSSConfig);
+    }, 200);
+
+    updateConfig();
+
+    return () => {
+      updateConfig.cancel();
+    };
+  }, [CSSConfig]);
+
   const handleCopy = useCallback(() => {
-    copyToClipboard(CSSConfig);
+    copyToClipboard(debouncedConfig);
   }, [CSSConfig]);
 
   return (
     <VStack gap="20px" width="100%" alignItems="start">
       <Button onClick={handleCopy}>Copy CSS</Button>
-      <Textarea fontFamily="monospace" width="100%" rows={10}>
-        {CSSConfig}
-      </Textarea>
+      <Textarea
+        fontFamily="monospace"
+        width="100%"
+        rows={10}
+        value={debouncedConfig}
+        readOnly
+      />
     </VStack>
   );
 };
--- src/ui/OutputConfigSettings.tsx
+++ src/ui/OutputConfigSettings.tsx
@@ -1,29 +1,45 @@
-import { FC, memo, useCallback, useMemo } from 'react';
+import { FC, memo, useCallback, useEffect, useState } from 'react';
 import { Button, Textarea, VStack } from '@chakra-ui/react';
 
 import { LayerConfig } from '../types';
 import { copyToClipboard } from '../utils/navigator';
+import { debounce } from 'lodash';
 
 type Props = {
   config: LayerConfig[];
 };
 
 const OutputConfigSettings: FC<Props> = ({ config }) => {
-  const formattedConfig = useMemo(
-    () => JSON.stringify(config, null, 2),
-    [config],
+  const [debouncedConfig, setDebouncedConfig] = useState(() =>
+    JSON.stringify(config, null, 2),
   );
 
+  useEffect(() => {
+    const updateConfig = debounce(() => {
+      setDebouncedConfig(JSON.stringify(config, null, 2));
+    }, 200);
+
+    updateConfig();
+
+    return () => {
+      updateConfig.cancel();
+    };
+  }, [config]);
+
   const handleCopy = useCallback(() => {
-    copyToClipboard(formattedConfig);
-  }, [formattedConfig]);
+    copyToClipboard(debouncedConfig);
+  }, [debouncedConfig]);
 
   return (
     <VStack gap="20px" width="100%" alignItems="start">
       <Button onClick={handleCopy}>Copy Config</Button>
-      <Textarea fontFamily="monospace" width="100%" rows={10}>
-        {formattedConfig}
-      </Textarea>
+      <Textarea
+        fontFamily="monospace"
+        width="100%"
+        rows={10}
+        value={debouncedConfig}
+        readOnly
+      />
     </VStack>
   );
 };
